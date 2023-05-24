575eb1608217ffa9c0e411a7a4abe4a34044b32d
Valeri Chichinadze <115809022+valerich95@users.noreply.github.com>
2023-05-24 09:38:55 +0400

    Update train.py

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 import os, sys, glob, time, pathlib, argparse
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '5'
 
+import pickle
+import matplotlib.pyplot as plt
+
 # Kerasa / TensorFlow
 from loss import depth_loss_function
 from utils import predict, save_images, load_test_data
@@ -10,6 +13,17 @@ from callbacks import get_nyu_callbacks
 
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import plot_model  # multi_gpu_model
+from tensorflow.keras.callbacks import Callback
+
+# this is a class for saving losses per batch
+class LossHistory(Callback):
+    def on_train_begin(self, logs={}):
+        self.losses = []
+        self.val_losses = []
+
+    def on_batch_end(self, batch, logs={}):
+        self.losses.append(logs.get('loss'))
+        self.val_losses.append(logs.get('val_loss'))
 
 # Argument Parser
 parser = argparse.ArgumentParser(description='High Quality Monocular Depth Estimation via Transfer Learning')
@@ -37,6 +51,18 @@ else:
 # Create the model
 model = create_model( existing=args.checkpoint )
 
+# if you see model_plot.png up4_convB, conv3 are last 2 layers based on your
+# goal to train last 2 layers and rest of the layers were freezed
+for layer in model.layers:
+    if layer.name == 'up4_convB':
+        layer.trainable = True
+
+    elif layer.name == 'conv3':
+        layer.trainable = True
+    
+    else:
+        layer.trainable = False
+
 # Data loaders
 if args.data == 'nyu': train_generator, test_generator = get_nyu_train_test_data( args.bs )
 if args.data == 'unreal': train_generator, test_generator = get_unreal_train_test_data( args.bs )
@@ -51,9 +77,9 @@ print('Output: ' + runPath)
  # (optional steps)
 if True:
     # Keep a copy of this training script and calling arguments
-    with open(__file__, 'r') as training_script: training_script_content = training_script.read()
-    training_script_content = '#' + str(sys.argv) + '\n' + training_script_content
-    with open(runPath+'/'+__file__, 'w') as training_script: training_script.write(training_script_content)
+    # with open(__file__, 'r') as training_script: training_script_content = training_script.read()
+    # training_script_content = '#' + str(sys.argv) + '\n' + training_script_content
+    # with open(runPath+'/'+__file__, 'w') as training_script: training_script.write(training_script_content)
 
     # Generate model plot
     plot_model(model, to_file=runPath+'/model_plot.png', show_shapes=True, show_layer_names=True)
@@ -68,7 +94,8 @@ basemodel = model  # https://discuss.tensorflow.org/t/multi-gpu-model/11778
 # if args.gpus > 1: model = multi_gpu_model(model, gpus=args.gpus)
 
 # Optimizer
-optimizer = Adam(lr=args.lr, amsgrad=True)
+# changing lr to learning_rate becauase of deprication
+optimizer = Adam(learning_rate=args.lr, amsgrad=True)
 
 # Compile the model
 print('\n\n\n', 'Compiling model..', runID, '\n\n\tGPU ' + (str(args.gpus)+' gpus' if args.gpus > 1 else args.gpuids)
@@ -82,8 +109,26 @@ callbacks = []
 if args.data == 'nyu': callbacks = get_nyu_callbacks(model, basemodel, train_generator, test_generator, load_test_data() if args.full else None , runPath)
 if args.data == 'unreal': callbacks = get_nyu_callbacks(model, basemodel, train_generator, test_generator, load_test_data() if args.full else None , runPath)
 
+# create an object from created class and adding it to the callbacks for saving losses
+history = LossHistory()
+callbacks.append(history)
+
 # Start training
+# changing fit_generator to fit becauase of deprication
 model.fit(train_generator, callbacks=callbacks, validation_data=test_generator, epochs=args.epochs, shuffle=True)
 
 # Save the final trained model:
 basemodel.save(runPath + '/model.h5')
+
+# save losses as pickle file
+with open(runPath + '/losses.pickle', 'wb') as f:
+    pickle.dump(history.history['loss'], f)
+
+
+# plot losses of every batch
+plt.plot(history.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('step')
+plt.savefig(runPath + '/plot.png')
+plt.show()
